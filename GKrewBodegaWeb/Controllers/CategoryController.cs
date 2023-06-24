8c960e7d29dcc6e3bbe93814421ec040c1f0721f
Bill <wspacoe@gmail.com>
2023-06-23 20:18:11 -0400

    Started Cat Controller

--- GKrewBodegaWeb/Controllers/CategoryController.cs
+++ GKrewBodegaWeb/Controllers/CategoryController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GKrewBodegaWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GKrewBodegaWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var objCategoryList = _db.Categories.ToList();
+
             return View();
         }
     }
